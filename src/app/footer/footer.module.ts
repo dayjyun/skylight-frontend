2dcb634042b6f05881422520ca62b3173fac25fb
dayjyun <kbarriosdev@gmail.com>
2023-06-05 16:05:41 -0500

    Updated footer CSS. Added

--- src/app/footer/footer.module.ts
+++ src/app/footer/footer.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { SocialIconsComponent } from './social-icons/social-icons.component';
+import { NavbarModule } from '../navbar/navbar.module';
 
 
 
@@ -11,7 +12,8 @@ import { SocialIconsComponent } from './social-icons/social-icons.component';
     SocialIconsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NavbarModule
   ],
   exports: [
     FooterComponent
--- src/app/footer/footer/footer.component.ts
+++ src/app/footer/footer/footer.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Dev {
+  portfolio: string,
+  github: string,
+  linkedIn: string,
+}
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  dev: Dev = {
+    portfolio: 'https://kbarrios.dev/',
+    github: 'https://github.com/dayjyun',
+    linkedIn: 'https://www.linkedin.com/in/kevinbarrios12/',
+  };
 }
