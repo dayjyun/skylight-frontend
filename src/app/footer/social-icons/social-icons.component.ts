56abc1d822e57ea24be8c4d78005d777352821a9
dayjyun <kbarriosdev@gmail.com>
2023-06-05 16:53:09 -0500

    Updated footer CSS

--- src/app/footer/social-icons/social-icons.component.ts
+++ src/app/footer/social-icons/social-icons.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+interface Dev {
+  portfolio: string;
+  github: string;
+  linkedIn: string;
+}
+
 @Component({
   selector: 'app-social-icons',
   templateUrl: './social-icons.component.html',
-  styleUrls: ['./social-icons.component.css']
+  styleUrls: ['./social-icons.component.css'],
 })
 export class SocialIconsComponent {
-
+  dev: Dev = {
+    portfolio: 'https://kbarrios.dev/',
+    github: 'https://github.com/dayjyun',
+    linkedIn: 'https://www.linkedin.com/in/kevinbarrios12/',
+  };
 }
