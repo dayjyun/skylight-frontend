ab74b7450e06666610919cf29baada1295f0a2ef
dayjyun <kbarriosdev@gmail.com>
2023-06-06 15:22:42 -0500

    Current progress with signup component

--- src/app/navbar/sign-up/sign-up.component.ts
+++ src/app/navbar/sign-up/sign-up.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
-  registerUserData: any = {}
+  registerUserData: any = {};
 
-  constructor(private userService: UsersService) { }
+  errorMessage: string = '';
 
-  ngOnInit(): void { }
+  constructor(private userService: UsersService, private router: Router) {}
 
+  ngOnInit(): void {}
+
+  /**
+   * Registers a new user by creating a user account through the user service.
+   * Redirects the
+   */
   registerUser() {
-    this.userService.createUser(this.registerUserData).subscribe(res => console.log(res)
-    )
+    // Clear previous error message
+    this.errorMessage = '';
+
+    // Perform form validation
+    if (
+      !this.registerUserData.name ||
+      !this.registerUserData.email ||
+      !this.registerUserData.password
+    ) {
+      this.errorMessage = 'Field is required.';
+      return;
+    }
+
+    this.userService.createUser(this.registerUserData).subscribe((data) => {
+      if (data) this.router.navigate(['/myProfile']);
+    });
   }
 }
