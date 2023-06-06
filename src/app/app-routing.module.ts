9134891a6e852c7c99d71b30aa16eb7d39da7dd0
dayjyun <kbarriosdev@gmail.com>
2023-06-06 01:13:46 -0500

    Removed unused icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MainPageComponent } from './main-page/main-page/main-page.component';
 
