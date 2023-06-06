8d41451182f9d73a29ddfe78aa776704bd861566
dayjyun <kbarriosdev@gmail.com>
2023-06-06 02:57:37 -0500

    Updated tickets service comments

--- src/app/services/tickets/tickets.service.ts
+++ src/app/services/tickets/tickets.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TicketsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  /**
+   * Functionality: Returns ticket details (Public/Private)
+   * Path: /api/tickets/{ticketId}
+   * @param ticketId is the ticket ID to search by
+   * @return a ticket
+   */
+  getTicketById(ticketId: number) {}
+
+  /**
+   * Functionality: Admin deletes ticket (Private)
+   * Path: /api/tickets/{ticketId}
+   * @param ticketId is the ticket ID to search by
+   * @return the deleted ticket data
+   */
+  deleteTicket(ticketId: number) {}
+
+  /**
+   * Functionality: User books ticket (Public)
+   * Path: /api/tickets/{ticketId}/bookFlight
+   * @param ticketId is the ticket ID to search by
+   * @return the booked ticket data
+   */
+  bookFlight(ticketId: number) {}
 }
