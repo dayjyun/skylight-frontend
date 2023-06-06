ac1999987f0b6fcccf5c628329121e05b9e78454
dayjyun <kbarriosdev@gmail.com>
2023-06-05 19:17:43 -0500

    Updated folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarModule } from 'src/app/navbar/navbar.module';
-import { HeaderModule } from './header/header.module';
+import { HeaderModule } from './main-page/header/header.module';
 import { FooterModule } from './footer/footer.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FlightsModule } from './flights/flights.module';
+import { NavbarModule } from './navbar/navbar.module';
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, NavbarModule, HeaderModule, FooterModule, FlightsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavbarModule,
+    HeaderModule,
+    FooterModule,
+    FlightsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/main-page/header/header.module.ts
+++ src/app/main-page/header/header.module.ts
@@ -3,21 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SplashImageComponent } from './splash-image/splash-image.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
-import { FormsModule } from '@angular/forms'
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    SplashImageComponent,
-    SearchBarComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-  ],
-  exports: [
-    HeaderComponent
-  ]
+  declarations: [HeaderComponent, SplashImageComponent, SearchBarComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [HeaderComponent],
 })
-export class HeaderModule { }
+export class HeaderModule {}
