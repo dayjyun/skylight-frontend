aca720a4d5703f2158b24c0396a9dd8f63501b4e
dayjyun <kbarriosdev@gmail.com>
2023-06-06 02:28:50 -0500

    Set up for User Service

--- src/app/services/users/users.service.ts
+++ src/app/services/users/users.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface User {
+  name: string,
+  email: string,
+  password: string,
+  isAdmin: boolean
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createUser(userObj: User) {
+    // /api/auth/register
+  }
+
+  loginUser() {
+    // /api/auth/login
+  }
+
+  getMyProfile() {
+    // /api/myProfile
+  }
+
+  updateMyProfile() {
+    // /api/myProfile
+  }
+
+  getMyTickets() {
+    // /api/myProfile/myTickets
+  }
+
+  flyTheSkies() {
+    // /api/myProfile/flyTheSkies
+  }
+
+  getScheduledFlights() {
+    // /api/myProfile/air
+  }
 }
