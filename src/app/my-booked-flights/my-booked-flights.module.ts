38d956bb13a70231bccf11f979a5ee61f595182f
dayjyun <kbarriosdev@gmail.com>
2023-06-08 11:16:10 -0500

    Removed my booked flights details

--- src/app/my-booked-flights/my-booked-flights.module.ts
+++ src/app/my-booked-flights/my-booked-flights.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyBookedFlightsComponent } from './my-booked-flights/my-booked-flights.component';
 import { MyBookedFlightsListComponent } from './my-booked-flights-list/my-booked-flights-list.component';
-import { MyBookedFlightDetailsComponent } from './my-booked-flight-details/my-booked-flight-details.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     MyBookedFlightsComponent,
     MyBookedFlightsListComponent,
-    MyBookedFlightDetailsComponent,
   ],
   imports: [CommonModule, RouterModule],
   exports: [MyBookedFlightsComponent],
