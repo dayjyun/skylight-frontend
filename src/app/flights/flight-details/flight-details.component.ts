3d07273dcfeb8e59e30737c6bbfdc7514682c097
dayjyun <kbarriosdev@gmail.com>
2023-06-08 12:52:09 -0500

    Added docstrings to flights

--- src/app/flights/flight-details/flight-details.component.ts
+++ src/app/flights/flight-details/flight-details.component.ts
@@ -8,13 +8,25 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./flight-details.component.css'],
 })
 export class FlightDetailsComponent implements OnInit {
+  /**
+   * Holds the flight data.
+   */
   flight: any;
 
+  /**
+   * Creates an instance of FlightDetailsComponent.
+   * @param route - The activated route containing the route parameters.
+   * @param flightsService - The service used for retrieving flight data.
+   */
   constructor(
     private route: ActivatedRoute,
     private flightsService: FlightsService
   ) {}
 
+  /**
+   * Lifecycle hook that is called after the component is initialized.
+   * Retrieves the flight data based on the route parameter and assigns it to the `flight` property.
+   */
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.flightsService.getAllFlights().subscribe((data: any) => {
