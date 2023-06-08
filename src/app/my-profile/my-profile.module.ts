72230c6c9736d6fe4be0b52705e1d8d60464bc7f
dayjyun <kbarriosdev@gmail.com>
2023-06-08 11:19:56 -0500

    Added doc strings to my profile, removed child routing

--- src/app/my-profile/my-profile.module.ts
+++ src/app/my-profile/my-profile.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { MyScheduledFlightsComponent } from './my-scheduled-flights/my-scheduled-flights.component';
 import { ButtonsModule } from '../buttons/buttons.module';
-import { MyProfileRoutingModule } from './my-profile-routing/my-profile-routing.module';
 import { MyBookedFlightsModule } from '../my-booked-flights/my-booked-flights.module';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { MyBookedFlightsModule } from '../my-booked-flights/my-booked-flights.mo
     CommonModule,
     ButtonsModule,
     MyBookedFlightsModule,
-    MyProfileRoutingModule,
   ],
 })
 export class MyProfileModule { }
