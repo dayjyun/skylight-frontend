ebac8e7faecd13a0deb2c9f4d038b25544699c6b
dayjyun <kbarriosdev@gmail.com>
2023-06-08 12:50:11 -0500

    Created docstrings for edit profile

--- src/app/buttons/sign-up-button/sign-up-button.component.ts
+++ src/app/buttons/sign-up-button/sign-up-button.component.ts
@@ -7,9 +7,4 @@ import { Router } from '@angular/router';
   styleUrls: ['./sign-up-button.component.css'],
 })
 export class SignUpButtonComponent {
-  constructor(private router: Router) {}
-
-  signUpPage() {
-    this.router.navigate(['/sign-up']);
-  }
 }
