06ca2db04cbb404d27b4e48862c5621a68c75ac3
dayjyun <kbarriosdev@gmail.com>
2023-06-08 19:39:28 -0500

    Added carousel for become a pilot

---
+++ src/app/become-a-pilot/pilot-carousel/pilot-carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pilot-carousel',
+  templateUrl: './pilot-carousel.component.html',
+  styleUrls: ['./pilot-carousel.component.css']
+})
+export class PilotCarouselComponent {
+
+}
