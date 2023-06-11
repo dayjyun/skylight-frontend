b0a5fecf794a9dbd6dd906e810660e0f5e08f680
dayjyun <kbarriosdev@gmail.com>
2023-06-11 12:24:29 -0500

    Created Schedule New Flight button and schedule new flight component

--- src/app/my-profile/my-profile.module.ts
+++ src/app/my-profile/my-profile.module.ts
@@ -4,16 +4,21 @@ import { MyProfileComponent } from './my-profile/my-profile.component';
 import { MyScheduledFlightsComponent } from './my-scheduled-flights/my-scheduled-flights.component';
 import { ButtonsModule } from '../buttons/buttons.module';
 import { MyBookedFlightsModule } from '../my-booked-flights/my-booked-flights.module';
+import { ScheduleFlightComponent } from './schedule-flight/schedule-flight.component';
 
 @NgModule({
   declarations: [
     MyProfileComponent,
-    MyScheduledFlightsComponent
+    MyScheduledFlightsComponent,
+    ScheduleFlightComponent
   ],
   imports: [
     CommonModule,
     ButtonsModule,
     MyBookedFlightsModule,
   ],
+  exports: [
+    ScheduleFlightComponent
+  ]
 })
 export class MyProfileModule { }
