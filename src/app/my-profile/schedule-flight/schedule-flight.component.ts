d2bfa53251cb9a0cb53faa3f69cf5149251aa06e
dayjyun <kbarriosdev@gmail.com>
2023-06-11 12:24:49 -0500

    Created Schedule New Flight button and schedule new flight component

---
+++ src/app/my-profile/schedule-flight/schedule-flight.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schedule-flight',
+  templateUrl: './schedule-flight.component.html',
+  styleUrls: ['./schedule-flight.component.css']
+})
+export class ScheduleFlightComponent {
+
+}
